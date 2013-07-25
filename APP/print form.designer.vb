d66f90947423a94ab706ee2942dab673fb52855d
imsinu9 <imsinu9@gmail.com>
2013-07-25 13:44:39 +0530

    Adding feature to print tickets

---
+++ APP/print form.designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class print
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(print))
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.txtpdate = New System.Windows.Forms.TextBox
+        Me.txtseat = New System.Windows.Forms.TextBox
+        Me.txtpaudi = New System.Windows.Forms.TextBox
+        Me.txtpclass = New System.Windows.Forms.TextBox
+        Me.txtptime = New System.Windows.Forms.TextBox
+        Me.txtpname = New System.Windows.Forms.TextBox
+        Me.txtamt = New System.Windows.Forms.TextBox
+        Me.Label9 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(161, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "INOX RAIPUR"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(83, 290)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Seat Name :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(84, 256)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Audi Name :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(119, 222)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Class :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(83, 188)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(81, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Show Time :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(74, 154)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(91, 16)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Movie Name :"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(124, 120)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(43, 16)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Date :"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(106, 376)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(59, 16)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Amount :"
+        '
+        'txtpdate
+        '
+        Me.txtpdate.BackColor = System.Drawing.Color.White
+        Me.txtpdate.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpdate.Location = New System.Drawing.Point(200, 119)
+        Me.txtpdate.Name = "txtpdate"
+        Me.txtpdate.ReadOnly = True
+        Me.txtpdate.Size = New System.Drawing.Size(172, 22)
+        Me.txtpdate.TabIndex = 8
+        Me.txtpdate.TabStop = False
+        '
+        'txtseat
+        '
+        Me.txtseat.BackColor = System.Drawing.Color.White
+        Me.txtseat.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtseat.Location = New System.Drawing.Point(200, 289)
+        Me.txtseat.Multiline = True
+        Me.txtseat.Name = "txtseat"
+        Me.txtseat.ReadOnly = True
+        Me.txtseat.Size = New System.Drawing.Size(172, 64)
+        Me.txtseat.TabIndex = 9
+        Me.txtseat.TabStop = False
+        '
+        'txtpaudi
+        '
+        Me.txtpaudi.BackColor = System.Drawing.Color.White
+        Me.txtpaudi.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpaudi.Location = New System.Drawing.Point(200, 257)
+        Me.txtpaudi.Name = "txtpaudi"
+        Me.txtpaudi.ReadOnly = True
+        Me.txtpaudi.Size = New System.Drawing.Size(172, 22)
+        Me.txtpaudi.TabIndex = 10
+        Me.txtpaudi.TabStop = False
+        '
+        'txtpclass
+        '
+        Me.txtpclass.BackColor = System.Drawing.Color.White
+        Me.txtpclass.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpclass.Location = New System.Drawing.Point(200, 221)
+        Me.txtpclass.Name = "txtpclass"
+        Me.txtpclass.ReadOnly = True
+        Me.txtpclass.Size = New System.Drawing.Size(172, 22)
+        Me.txtpclass.TabIndex = 11
+        Me.txtpclass.TabStop = False
+        '
+        'txtptime
+        '
+        Me.txtptime.BackColor = System.Drawing.Color.White
+        Me.txtptime.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtptime.Location = New System.Drawing.Point(200, 187)
+        Me.txtptime.Name = "txtptime"
+        Me.txtptime.ReadOnly = True
+        Me.txtptime.Size = New System.Drawing.Size(172, 22)
+        Me.txtptime.TabIndex = 12
+        Me.txtptime.TabStop = False
+        '
+        'txtpname
+        '
+        Me.txtpname.BackColor = System.Drawing.Color.White
+        Me.txtpname.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpname.Location = New System.Drawing.Point(200, 153)
+        Me.txtpname.Name = "txtpname"
+        Me.txtpname.ReadOnly = True
+        Me.txtpname.Size = New System.Drawing.Size(172, 22)
+        Me.txtpname.TabIndex = 13
+        Me.txtpname.TabStop = False
+        '
+        'txtamt
+        '
+        Me.txtamt.BackColor = System.Drawing.Color.White
+        Me.txtamt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtamt.Location = New System.Drawing.Point(229, 375)
+        Me.txtamt.Name = "txtamt"
+        Me.txtamt.ReadOnly = True
+        Me.txtamt.Size = New System.Drawing.Size(143, 22)
+        Me.txtamt.TabIndex = 14
+        Me.txtamt.TabStop = False
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(198, 376)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(28, 16)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "Rs."
+        '
+        'Button1
+        '
+        Me.Button1.Image = Global.Box_Office.My.Resources.Resources.print_icon
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.Location = New System.Drawing.Point(169, 448)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 34)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "   PRINT"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Office.My.Resources.Resources.Inox_Leisure
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 111)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 16
+        Me.PictureBox1.TabStop = False
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'print
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(446, 535)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtamt)
+        Me.Controls.Add(Me.txtpname)
+        Me.Controls.Add(Me.txtptime)
+        Me.Controls.Add(Me.txtpclass)
+        Me.Controls.Add(Me.txtpaudi)
+        Me.Controls.Add(Me.txtseat)
+        Me.Controls.Add(Me.txtpdate)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "print"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "INOX"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents txtpdate As System.Windows.Forms.TextBox
+    Friend WithEvents txtseat As System.Windows.Forms.TextBox
+    Friend WithEvents txtpaudi As System.Windows.Forms.TextBox
+    Friend WithEvents txtpclass As System.Windows.Forms.TextBox
+    Friend WithEvents txtptime As System.Windows.Forms.TextBox
+    Friend WithEvents txtpname As System.Windows.Forms.TextBox
+    Friend WithEvents txtamt As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PrintForm1 As Microsoft.VisualBasic.PowerPacks.Printing.PrintForm
+End Class
---
+++ APP/print form.vb
@@ -0,0 +1,24 @@
+﻿Public Class print
+
+    Private Sub print_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        PrintForm1.Print()
+        Welcome.Show()
+        Me.Close()
+
+
+
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(ByVal sender As System.Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs)
+
+    End Sub
+
+    
+End Class

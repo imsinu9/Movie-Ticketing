6594ae33b9cc33c02e4775488da5990a06e867cc
imsinu9 <imsinu9@gmail.com>
2013-07-25 13:44:57 +0530

    Date updation

---
+++ APP/update date.designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class update_date
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dtpold = New System.Windows.Forms.DateTimePicker
+        Me.dtpnew = New System.Windows.Forms.DateTimePicker
+        Me.ListView1 = New System.Windows.Forms.ListView
+        Me.ColumnHeader1 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader2 = New System.Windows.Forms.ColumnHeader
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.btnok = New System.Windows.Forms.Button
+        Me.btnexit = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dtpold
+        '
+        Me.dtpold.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpold.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpold.Location = New System.Drawing.Point(141, 183)
+        Me.dtpold.Name = "dtpold"
+        Me.dtpold.Size = New System.Drawing.Size(136, 22)
+        Me.dtpold.TabIndex = 0
+        '
+        'dtpnew
+        '
+        Me.dtpnew.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpnew.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpnew.Location = New System.Drawing.Point(141, 249)
+        Me.dtpnew.Name = "dtpnew"
+        Me.dtpnew.Size = New System.Drawing.Size(136, 22)
+        Me.dtpnew.TabIndex = 1
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.ListView1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListView1.Location = New System.Drawing.Point(304, 110)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(371, 335)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Date"
+        Me.ColumnHeader1.Width = 174
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Show Time"
+        Me.ColumnHeader2.Width = 168
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(49, 189)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Old Date :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(43, 255)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "New Date :"
+        '
+        'btnok
+        '
+        Me.btnok.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnok.Image = Global.Box_Office.My.Resources.Resources.Symbol_HookOK_18px
+        Me.btnok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnok.Location = New System.Drawing.Point(166, 475)
+        Me.btnok.Name = "btnok"
+        Me.btnok.Size = New System.Drawing.Size(87, 36)
+        Me.btnok.TabIndex = 5
+        Me.btnok.Text = "OK"
+        Me.btnok.UseVisualStyleBackColor = True
+        '
+        'btnexit
+        '
+        Me.btnexit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnexit.Image = Global.Box_Office.My.Resources.Resources.cancelIcon
+        Me.btnexit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnexit.Location = New System.Drawing.Point(456, 475)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(87, 36)
+        Me.btnexit.TabIndex = 6
+        Me.btnexit.Text = "   EXIT"
+        Me.btnexit.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(300, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(132, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "UPDATE DATE"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Office.My.Resources.Resources.Inox_Leisure
+        Me.PictureBox1.Location = New System.Drawing.Point(27, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(133, 112)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'update_date
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(714, 533)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btnok)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.dtpnew)
+        Me.Controls.Add(Me.dtpold)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "update_date"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "UPDATE DATE"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dtpold As System.Windows.Forms.DateTimePicker
+    Friend WithEvents dtpnew As System.Windows.Forms.DateTimePicker
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btnok As System.Windows.Forms.Button
+    Friend WithEvents btnexit As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+End Class
---
+++ APP/update date.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Data.SqlClient
+Public Class update_date
+    
+
+    Private Sub update_date_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Connect()
+    End Sub
+
+    Private Sub dtpold_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dtpold.ValueChanged
+        LoadData()
+    End Sub
+
+    Private Sub LoadData()
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+        Dim Lv As ListViewItem
+
+        s_sql = "select ms.mdate,st.Show_Time from Movie_Schedule ms,Show_Time st where ms.Show_ID =st.Show_ID and ms.mDate ='" & dtpold.Text & "'"
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_Reader = o_cmd.ExecuteReader()
+        ListView1.Items.Clear()
+
+        While o_Reader.Read
+            Lv = ListView1.Items.Add(o_Reader("mdate"))
+            Lv.SubItems.Add(o_Reader("Show_Time"))
+        End While
+        o_Reader.Close()
+    End Sub
+
+    Private Sub btnok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnok.Click
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+        Dim s_sql1 As String
+        Dim o_cmd1 As SqlCommand
+        Dim j As Integer
+        Dim showid(24) As Integer
+
+
+        s_sql = "Select st.Show_ID from Movie_Schedule ms,Show_Time st where ms.Show_ID=st.Show_ID and ms.mdate='" & dtpold.Text & "'"
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_Reader = o_cmd.ExecuteReader()
+        While o_Reader.Read
+            showid(j) = o_Reader(0)
+            j = j + 1
+        End While
+        o_Reader.Close()
+        For k As Integer = LBound(showid) To UBound(showid)
+            s_sql1 = "Update Movie_Schedule set mdate='" & DateValue(dtpnew.Value) & "' where Show_ID='" & showid(k) & "'"
+            o_cmd1 = New SqlCommand(s_sql1, o_con)
+            o_cmd1.ExecuteNonQuery()
+        Next
+
+        LoadData()
+    End Sub
+
+    Private Sub btnexit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnexit.Click
+        Me.Close()
+    End Sub
+End Class

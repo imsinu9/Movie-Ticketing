e3eed9225f8428870aeb652323d42dea963f1951
imsinu9 <imsinu9@gmail.com>
2013-07-25 13:38:41 +0530

    Adding login page

---
+++ APP/LogIn.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LogIn
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtusername = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtpassword = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.btnclear = New System.Windows.Forms.Button
+        Me.btncancel = New System.Windows.Forms.Button
+        Me.btnok = New System.Windows.Forms.Button
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(74, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Username :"
+        '
+        'txtusername
+        '
+        Me.txtusername.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtusername.Location = New System.Drawing.Point(185, 109)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(151, 22)
+        Me.txtusername.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(77, 161)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password :"
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpassword.Location = New System.Drawing.Point(185, 158)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtpassword.Size = New System.Drawing.Size(151, 22)
+        Me.txtpassword.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(173, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "LOGIN"
+        '
+        'btnclear
+        '
+        Me.btnclear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclear.Image = Global.Box_Office.My.Resources.Resources.icon_refresh
+        Me.btnclear.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnclear.Location = New System.Drawing.Point(158, 238)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(95, 33)
+        Me.btnclear.TabIndex = 6
+        Me.btnclear.Text = "     CLEAR"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'btncancel
+        '
+        Me.btncancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncancel.Image = Global.Box_Office.My.Resources.Resources.cancelIcon
+        Me.btncancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btncancel.Location = New System.Drawing.Point(281, 238)
+        Me.btncancel.Name = "btncancel"
+        Me.btncancel.Size = New System.Drawing.Size(95, 33)
+        Me.btncancel.TabIndex = 7
+        Me.btncancel.Text = "     CANCEL"
+        Me.btncancel.UseVisualStyleBackColor = True
+        '
+        'btnok
+        '
+        Me.btnok.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnok.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnok.Image = Global.Box_Office.My.Resources.Resources.Symbol_HookOK_18px
+        Me.btnok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnok.Location = New System.Drawing.Point(35, 238)
+        Me.btnok.Name = "btnok"
+        Me.btnok.Size = New System.Drawing.Size(95, 33)
+        Me.btnok.TabIndex = 5
+        Me.btnok.Text = "OK"
+        Me.btnok.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Office.My.Resources.Resources.Inox_Leisure
+        Me.PictureBox1.Location = New System.Drawing.Point(43, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(83, 81)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'LogIn
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(411, 301)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btncancel)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.btnok)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "LogIn"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "INOX"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtusername As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtpassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnok As System.Windows.Forms.Button
+    Friend WithEvents btnclear As System.Windows.Forms.Button
+    Friend WithEvents btncancel As System.Windows.Forms.Button
+End Class
---
+++ APP/LogIn.vb
@@ -0,0 +1,94 @@
+﻿Imports System.Data.SqlClient
+
+Public Class LogIn
+
+
+
+    Private Sub btncancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncancel.Click
+
+        Welcome.Show()
+        Me.Close()
+
+    End Sub
+
+
+    Private Sub LogIn_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Connect()
+        txtusername.Focus()
+        btnok.Enabled = False
+
+    End Sub
+
+    Private Sub txtusername_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtusername.GotFocus
+
+        Welcome.Hide()
+
+    End Sub
+
+    Private Sub btnok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnok.Click
+
+
+        Dim s_Sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+
+        s_Sql = "Select password from login where username='" & txtusername.Text & "' and password='" & txtpassword.Text & "' "
+        o_cmd = New SqlCommand(s_Sql, o_con)
+        o_Reader = o_cmd.ExecuteReader
+
+        If o_Reader.Read = False Then
+            MsgBox("Enter correct Username & Password")
+            txtusername.Text = ""
+            txtpassword.Text = ""
+            txtusername.Focus()
+            o_Reader.Close()
+            Exit Sub
+        End If
+
+
+        Welcome.Show()
+        Welcome.Label1.Visible = True
+        Welcome.LinkLabel1.Visible = True
+        Welcome.LinkLabel2.Visible = True
+        Welcome.Button2.Visible = False
+        Welcome.PictureBox2.Visible = False
+        Welcome.PictureBox1.Visible = True
+        Welcome.Button1.Visible = False
+        Welcome.Button3.Visible = True
+        Welcome.Close.Visible = False
+        Welcome.LinkLabel5.Visible = True
+
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnclear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnclear.Click
+        txtusername.Text = ""
+        txtpassword.Text = ""
+        txtusername.Focus()
+    End Sub
+    
+
+   
+
+    Private Sub txtusername_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtusername.TextChanged
+        If txtusername.Text = "" Or txtpassword.Text = "" Then
+            btnok.Enabled = False
+        Else
+            btnok.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub txtpassword_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtpassword.TextChanged
+        If txtusername.Text = "" Or txtpassword.Text = "" Then
+            btnok.Enabled = False
+        Else
+            btnok.Enabled = True
+        End If
+    End Sub
+
+    
+End Class

e962d43de47b0752e5392dd9fe49bd5dd269ad9f
imsinu9 <imsinu9@gmail.com>
2013-07-25 13:31:23 +0530

    Giving Admin UI to schedule movies

---
+++ APP/AddMovie.designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AddMovie
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.txtmoviename = New System.Windows.Forms.TextBox
+        Me.cborating = New System.Windows.Forms.ComboBox
+        Me.dtprelsdate = New System.Windows.Forms.DateTimePicker
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.cboactive = New System.Windows.Forms.ComboBox
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.btncancel = New System.Windows.Forms.Button
+        Me.btnclear = New System.Windows.Forms.Button
+        Me.btnok = New System.Windows.Forms.Button
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(80, 136)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Movie Name :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(74, 180)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Release Date :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(114, 225)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Rating :"
+        '
+        'txtmoviename
+        '
+        Me.txtmoviename.Location = New System.Drawing.Point(209, 136)
+        Me.txtmoviename.MaxLength = 49
+        Me.txtmoviename.Name = "txtmoviename"
+        Me.txtmoviename.Size = New System.Drawing.Size(156, 20)
+        Me.txtmoviename.TabIndex = 2
+        '
+        'cborating
+        '
+        Me.cborating.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cborating.FormattingEnabled = True
+        Me.cborating.Items.AddRange(New Object() {"(U/A)", "(A)", "(U)"})
+        Me.cborating.Location = New System.Drawing.Point(209, 223)
+        Me.cborating.Name = "cborating"
+        Me.cborating.Size = New System.Drawing.Size(156, 21)
+        Me.cborating.TabIndex = 6
+        '
+        'dtprelsdate
+        '
+        Me.dtprelsdate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtprelsdate.Location = New System.Drawing.Point(209, 180)
+        Me.dtprelsdate.Name = "dtprelsdate"
+        Me.dtprelsdate.Size = New System.Drawing.Size(156, 20)
+        Me.dtprelsdate.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(116, 271)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 17)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Active :"
+        '
+        'cboactive
+        '
+        Me.cboactive.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboactive.FormattingEnabled = True
+        Me.cboactive.Items.AddRange(New Object() {"Yes", "No"})
+        Me.cboactive.Location = New System.Drawing.Point(209, 267)
+        Me.cboactive.Name = "cboactive"
+        Me.cboactive.Size = New System.Drawing.Size(156, 21)
+        Me.cboactive.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(178, 39)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(109, 20)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "ADD MOVIE"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Office.My.Resources.Resources.Inox_Leisure
+        Me.PictureBox1.Location = New System.Drawing.Point(31, 20)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(86, 88)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'btncancel
+        '
+        Me.btncancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncancel.Image = Global.Box_Office.My.Resources.Resources.cancelIcon
+        Me.btncancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btncancel.Location = New System.Drawing.Point(295, 326)
+        Me.btncancel.Name = "btncancel"
+        Me.btncancel.Size = New System.Drawing.Size(95, 33)
+        Me.btncancel.TabIndex = 11
+        Me.btncancel.Text = "     CANCEL"
+        Me.btncancel.UseVisualStyleBackColor = True
+        '
+        'btnclear
+        '
+        Me.btnclear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclear.Image = Global.Box_Office.My.Resources.Resources.icon_refresh
+        Me.btnclear.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnclear.Location = New System.Drawing.Point(172, 326)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(95, 33)
+        Me.btnclear.TabIndex = 10
+        Me.btnclear.Text = "     CLEAR"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'btnok
+        '
+        Me.btnok.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnok.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnok.Image = Global.Box_Office.My.Resources.Resources.Symbol_HookOK_18px
+        Me.btnok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnok.Location = New System.Drawing.Point(49, 326)
+        Me.btnok.Name = "btnok"
+        Me.btnok.Size = New System.Drawing.Size(95, 33)
+        Me.btnok.TabIndex = 9
+        Me.btnok.Text = "OK"
+        Me.btnok.UseVisualStyleBackColor = True
+        '
+        'AddMovie
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(439, 370)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btncancel)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.btnok)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cboactive)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.dtprelsdate)
+        Me.Controls.Add(Me.cborating)
+        Me.Controls.Add(Me.txtmoviename)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "AddMovie"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ADD MOVIE"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtmoviename As System.Windows.Forms.TextBox
+    Friend WithEvents cborating As System.Windows.Forms.ComboBox
+    Friend WithEvents dtprelsdate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cboactive As System.Windows.Forms.ComboBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents btncancel As System.Windows.Forms.Button
+    Friend WithEvents btnclear As System.Windows.Forms.Button
+    Friend WithEvents btnok As System.Windows.Forms.Button
+End Class
---
+++ APP/AddMovie.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Data.SqlClient
+Public Class AddMovie
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtmoviename.TextChanged
+        If txtmoviename.Text = "" Then
+            btnok.Enabled = False
+        Else
+            btnok.Enabled = True
+
+        End If
+    End Sub
+
+    Private Sub btnclear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnclear.Click
+        txtmoviename.Text = ""
+        cborating.SelectedIndex = 0
+        cboactive.SelectedIndex = 0
+        dtprelsdate.Value = Today
+        txtmoviename.Focus()
+
+    End Sub
+
+    Private Sub btncancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncancel.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub movieedit_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Connect()
+        btnok.Enabled = False
+
+    End Sub
+
+    Private Sub btnok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnok.Click
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+
+        s_sql = "Insert into Movie_Details (Movie_Name,Release_Date,Active) values ('" & txtmoviename.Text & cborating.Text & "','" & dtprelsdate.Text & "','" & cboactive.Text & "')"
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_cmd.ExecuteNonQuery()
+        MsgBox("Movie Added")
+
+        administator.Show()
+        Me.Close()
+    End Sub
+End Class

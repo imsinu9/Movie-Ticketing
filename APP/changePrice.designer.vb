f86fa11b5cde558c1bb17c13759e8c0abc57e0c7
imsinu9 <imsinu9@gmail.com>
2013-07-25 13:42:00 +0530

    Adding price change feature depending upon audi

---
+++ APP/changePrice.designer.vb
@@ -0,0 +1,289 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class changePrice
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.dtpk = New System.Windows.Forms.DateTimePicker
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.cboMovieName = New System.Windows.Forms.ComboBox
+        Me.cboShowTime = New System.Windows.Forms.ComboBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.txtclass = New System.Windows.Forms.TextBox
+        Me.txtprice = New System.Windows.Forms.TextBox
+        Me.ListView1 = New System.Windows.Forms.ListView
+        Me.ColumnHeader3 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader1 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader2 = New System.Windows.Forms.ColumnHeader
+        Me.btnok = New System.Windows.Forms.Button
+        Me.btnexit = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.Label6 = New System.Windows.Forms.Label
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(114, 149)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Date :"
+        '
+        'dtpk
+        '
+        Me.dtpk.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpk.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpk.Location = New System.Drawing.Point(183, 149)
+        Me.dtpk.Margin = New System.Windows.Forms.Padding(2)
+        Me.dtpk.Name = "dtpk"
+        Me.dtpk.Size = New System.Drawing.Size(194, 22)
+        Me.dtpk.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(69, 203)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Movie Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(76, 257)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Show Time :"
+        '
+        'cboMovieName
+        '
+        Me.cboMovieName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboMovieName.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboMovieName.FormattingEnabled = True
+        Me.cboMovieName.Location = New System.Drawing.Point(183, 202)
+        Me.cboMovieName.Margin = New System.Windows.Forms.Padding(2)
+        Me.cboMovieName.Name = "cboMovieName"
+        Me.cboMovieName.Size = New System.Drawing.Size(194, 24)
+        Me.cboMovieName.TabIndex = 4
+        '
+        'cboShowTime
+        '
+        Me.cboShowTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboShowTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboShowTime.FormattingEnabled = True
+        Me.cboShowTime.Location = New System.Drawing.Point(183, 257)
+        Me.cboShowTime.Margin = New System.Windows.Forms.Padding(2)
+        Me.cboShowTime.Name = "cboShowTime"
+        Me.cboShowTime.Size = New System.Drawing.Size(194, 24)
+        Me.cboShowTime.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(109, 311)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Class :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(114, 365)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 16)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Price :"
+        '
+        'txtclass
+        '
+        Me.txtclass.BackColor = System.Drawing.Color.White
+        Me.txtclass.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtclass.Location = New System.Drawing.Point(183, 312)
+        Me.txtclass.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtclass.Name = "txtclass"
+        Me.txtclass.ReadOnly = True
+        Me.txtclass.Size = New System.Drawing.Size(123, 22)
+        Me.txtclass.TabIndex = 8
+        '
+        'txtprice
+        '
+        Me.txtprice.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtprice.Location = New System.Drawing.Point(183, 365)
+        Me.txtprice.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtprice.Name = "txtprice"
+        Me.txtprice.Size = New System.Drawing.Size(123, 22)
+        Me.txtprice.TabIndex = 9
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader3, Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(480, 149)
+        Me.ListView1.Margin = New System.Windows.Forms.Padding(2)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(270, 170)
+        Me.ListView1.TabIndex = 10
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Class ID"
+        Me.ColumnHeader3.Width = 54
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Class"
+        Me.ColumnHeader1.Width = 104
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Price"
+        Me.ColumnHeader2.Width = 92
+        '
+        'btnok
+        '
+        Me.btnok.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnok.Image = Global.Box_Office.My.Resources.Resources.Symbol_HookOK_18px
+        Me.btnok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnok.Location = New System.Drawing.Point(206, 412)
+        Me.btnok.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnok.Name = "btnok"
+        Me.btnok.Size = New System.Drawing.Size(106, 38)
+        Me.btnok.TabIndex = 11
+        Me.btnok.Text = "OK"
+        Me.btnok.UseVisualStyleBackColor = True
+        '
+        'btnexit
+        '
+        Me.btnexit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnexit.Image = Global.Box_Office.My.Resources.Resources.cancelIcon
+        Me.btnexit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnexit.Location = New System.Drawing.Point(459, 412)
+        Me.btnexit.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(106, 38)
+        Me.btnexit.TabIndex = 12
+        Me.btnexit.Text = "Exit"
+        Me.btnexit.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(338, 319)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(76, 22)
+        Me.TextBox1.TabIndex = 13
+        Me.TextBox1.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Office.My.Resources.Resources.Inox_Leisure
+        Me.PictureBox1.Location = New System.Drawing.Point(20, 11)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(108, 102)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 15
+        Me.PictureBox1.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(305, 42)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(143, 20)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "CHANGE PRICE"
+        '
+        'changePrice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(770, 479)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btnok)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.txtprice)
+        Me.Controls.Add(Me.txtclass)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cboShowTime)
+        Me.Controls.Add(Me.cboMovieName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.dtpk)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "changePrice"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CHANGE PRICE"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents dtpk As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents cboMovieName As System.Windows.Forms.ComboBox
+    Friend WithEvents cboShowTime As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtclass As System.Windows.Forms.TextBox
+    Friend WithEvents txtprice As System.Windows.Forms.TextBox
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btnok As System.Windows.Forms.Button
+    Friend WithEvents btnexit As System.Windows.Forms.Button
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+End Class
---
+++ APP/changePrice.vb
@@ -0,0 +1,120 @@
+﻿Imports System.Data.SqlClient
+Public Class changePrice
+
+    Private Sub changePrice_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Connect()
+    End Sub
+    Private Sub loaddata()
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_reader As SqlDataReader
+        Dim lv As ListViewItem
+        s_sql = "SELECT Audi_Class.Class_Name,Audi_Class.Price,audi_class.class_id FROM Show_Time INNER JOIN Audi_Class ON Show_Time.Show_ID = Audi_Class.Show_ID inner join Movie_Schedule on Movie_Schedule.Show_ID= Show_Time.Show_ID where Show_Time.Show_Time='" & cboShowTime.Text & "'and movie_schedule.mdate='" & dtpk.Text & "'"
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_reader = o_cmd.ExecuteReader
+        ListView1.Items.Clear()
+
+        While o_reader.Read
+            lv = ListView1.Items.Add(o_reader("class_id") & "")
+            lv.SubItems.Add(o_reader("class_name") & "")
+            lv.SubItems.Add(o_reader("Price") & "")
+        End While
+        o_reader.Close()
+
+    End Sub
+
+
+    Private Sub DateTimePicker1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        
+
+    End Sub
+
+    Private Sub Fillmovie()
+
+        Dim s_Sql As String
+        Dim o_adapter As SqlDataAdapter
+        Dim o_data As Data.DataSet
+
+
+        s_Sql = "SELECT Movie_Details.Movie_Name FROM  Movie_Details INNER Join Movie_Schedule ON Movie_Details.Movie_ID = Movie_Schedule.Movie_ID where movie_schedule.mdate='" & dtpk.Text & "' and Movie_Details.Active='Yes' group by movie_details.movie_name"
+        o_adapter = New SqlDataAdapter(s_Sql, o_con)
+        o_data = New Data.DataSet
+        o_adapter.Fill(o_data)
+
+        cboMovieName.DataSource = o_data.Tables(0)
+        cboMovieName.DisplayMember = o_data.Tables(0).Columns(0).ColumnName
+        cboMovieName.ValueMember = o_data.Tables(0).Columns(0).ColumnName
+
+    End Sub
+
+    Private Sub Fillshowtime()
+        Dim s_Sql As String
+        Dim o_adapter As SqlDataAdapter
+        Dim o_data As Data.DataSet
+
+        s_Sql = "SELECT Show_Time.Show_Time AS Expr1 FROM  Movie_Schedule INNER JOIN Show_Time ON Movie_Schedule.Show_ID = Show_Time.Show_ID INNER JOIN Movie_Details ON Movie_Schedule.Movie_ID = Movie_Details.Movie_ID where Movie_Schedule.Show_ID<=24 and Movie_Details.Movie_Name='" & cboMovieName.SelectedValue & "'"
+
+        o_adapter = New SqlDataAdapter(s_Sql, o_con)
+        o_data = New Data.DataSet
+        o_adapter.Fill(o_data)
+        cboShowTime.DataSource = o_data.Tables(0)
+        cboShowTime.DisplayMember = o_data.Tables(0).Columns(0).ColumnName
+        cboShowTime.ValueMember = o_data.Tables(0).Columns(0).ColumnName
+
+    End Sub
+
+
+    Private Sub cboMovieName_DropDownClosed(ByVal sender As Object, ByVal e As System.EventArgs) Handles cboMovieName.DropDownClosed
+        Fillshowtime()
+    End Sub
+
+    Private Sub cboShowTime_DropDownClosed(ByVal sender As Object, ByVal e As System.EventArgs) Handles cboShowTime.DropDownClosed
+        loaddata()
+    End Sub
+
+
+    Private Sub ListView1_DoubleClick1(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListView1.DoubleClick
+        Dim user As Integer
+        user = ListView1.SelectedItems(0).Text
+
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_reader As SqlDataReader
+
+        s_sql = "SELECT class_name,price from audi_class where class_id=" & Val(user) & ""
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_reader = o_cmd.ExecuteReader
+
+        If o_reader.Read Then
+            TextBox1.Text = Val(user)
+            txtclass.Text = o_reader("class_name") & ""
+            txtprice.Text = o_reader("Price") & ""
+        End If
+
+        o_reader.Close()
+
+    End Sub
+
+    Private Sub btnok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnok.Click
+        Dim s_Sql As String
+        Dim o_cmd As SqlCommand
+
+        s_Sql = "Update audi_class set price=@price where class_id=" & Val(TextBox1.Text)
+        o_cmd = New SqlCommand(s_Sql, o_con)
+        o_cmd.Parameters.Add(New SqlParameter("@price", Val(txtprice.Text)))
+        o_cmd.ExecuteNonQuery()
+
+        MsgBox("Record Updated")
+        loaddata()
+    End Sub
+
+    Private Sub btnexit_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnexit.Click
+        Me.Close()
+        dtpk.Focus()
+
+    End Sub
+
+    Private Sub dtpk_ValueChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles dtpk.ValueChanged
+        Fillmovie()
+    End Sub
+End Class

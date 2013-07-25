6892125094ce6e4b6cc800394ef1d079e642790f
imsinu9 <imsinu9@gmail.com>
2013-07-25 13:36:07 +0530

    UI to Alter movie in theatre by admin

---
+++ APP/Changemovieshows.designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Changemovieshows
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.btncancel = New System.Windows.Forms.Button
+        Me.btnok = New System.Windows.Forms.Button
+        Me.ListView1 = New System.Windows.Forms.ListView
+        Me.ColumnHeader1 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader2 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader3 = New System.Windows.Forms.ColumnHeader
+        Me.dtp1 = New System.Windows.Forms.DateTimePicker
+        Me.txt1 = New System.Windows.Forms.TextBox
+        Me.txtid = New System.Windows.Forms.TextBox
+        Me.cbomoviename = New System.Windows.Forms.ComboBox
+        Me.cbomovie1 = New System.Windows.Forms.ComboBox
+        Me.ListView2 = New System.Windows.Forms.ListView
+        Me.ColumnHeader4 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader5 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader6 = New System.Windows.Forms.ColumnHeader
+        Me.txtmid = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Office.My.Resources.Resources.Inox_Leisure
+        Me.PictureBox1.Location = New System.Drawing.Point(46, 10)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(86, 88)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(341, 37)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(217, 20)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "CHANGE MOVIE SHOWS"
+        '
+        'btncancel
+        '
+        Me.btncancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncancel.Image = Global.Box_Office.My.Resources.Resources.cancelIcon
+        Me.btncancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btncancel.Location = New System.Drawing.Point(524, 472)
+        Me.btncancel.Name = "btncancel"
+        Me.btncancel.Size = New System.Drawing.Size(95, 33)
+        Me.btncancel.TabIndex = 14
+        Me.btncancel.Text = "     CANCEL"
+        Me.btncancel.UseVisualStyleBackColor = True
+        '
+        'btnok
+        '
+        Me.btnok.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnok.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnok.Image = Global.Box_Office.My.Resources.Resources.Symbol_HookOK_18px
+        Me.btnok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnok.Location = New System.Drawing.Point(278, 472)
+        Me.btnok.Name = "btnok"
+        Me.btnok.Size = New System.Drawing.Size(95, 33)
+        Me.btnok.TabIndex = 13
+        Me.btnok.Text = "OK"
+        Me.btnok.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(37, 239)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(389, 216)
+        Me.ListView1.Sorting = System.Windows.Forms.SortOrder.Ascending
+        Me.ListView1.TabIndex = 11
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Show ID"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Show Name"
+        Me.ColumnHeader2.Width = 100
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Movie Name"
+        Me.ColumnHeader3.Width = 220
+        '
+        'dtp1
+        '
+        Me.dtp1.CustomFormat = "yyyy-mm-dd"
+        Me.dtp1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtp1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtp1.Location = New System.Drawing.Point(101, 123)
+        Me.dtp1.MinDate = New Date(2010, 1, 1, 0, 0, 0, 0)
+        Me.dtp1.Name = "dtp1"
+        Me.dtp1.Size = New System.Drawing.Size(137, 22)
+        Me.dtp1.TabIndex = 2
+        '
+        'txt1
+        '
+        Me.txt1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt1.Location = New System.Drawing.Point(455, 124)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(114, 22)
+        Me.txt1.TabIndex = 8
+        '
+        'txtid
+        '
+        Me.txtid.Location = New System.Drawing.Point(715, 78)
+        Me.txtid.Name = "txtid"
+        Me.txtid.Size = New System.Drawing.Size(130, 20)
+        Me.txtid.TabIndex = 9
+        Me.txtid.Visible = False
+        '
+        'cbomoviename
+        '
+        Me.cbomoviename.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbomoviename.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbomoviename.FormattingEnabled = True
+        Me.cbomoviename.Location = New System.Drawing.Point(174, 182)
+        Me.cbomoviename.Name = "cbomoviename"
+        Me.cbomoviename.Size = New System.Drawing.Size(190, 24)
+        Me.cbomoviename.TabIndex = 4
+        '
+        'cbomovie1
+        '
+        Me.cbomovie1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbomovie1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbomovie1.FormattingEnabled = True
+        Me.cbomovie1.Location = New System.Drawing.Point(618, 184)
+        Me.cbomovie1.Name = "cbomovie1"
+        Me.cbomovie1.Size = New System.Drawing.Size(190, 24)
+        Me.cbomovie1.TabIndex = 6
+        '
+        'ListView2
+        '
+        Me.ListView2.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6})
+        Me.ListView2.FullRowSelect = True
+        Me.ListView2.Location = New System.Drawing.Point(473, 239)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(389, 216)
+        Me.ListView2.Sorting = System.Windows.Forms.SortOrder.Ascending
+        Me.ListView2.TabIndex = 12
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        Me.ListView2.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Show ID"
+        Me.ColumnHeader4.Width = 64
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Show Name"
+        Me.ColumnHeader5.Width = 109
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Movie Name"
+        Me.ColumnHeader6.Width = 207
+        '
+        'txtmid
+        '
+        Me.txtmid.Location = New System.Drawing.Point(715, 123)
+        Me.txtmid.Name = "txtmid"
+        Me.txtmid.Size = New System.Drawing.Size(130, 20)
+        Me.txtmid.TabIndex = 10
+        Me.txtmid.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(91, 187)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "From Movie :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(549, 185)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "To Movie :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(352, 127)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 16)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Show Time :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(52, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 16)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Date :"
+        '
+        'Changemovieshows
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(899, 530)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtmid)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.cbomovie1)
+        Me.Controls.Add(Me.cbomoviename)
+        Me.Controls.Add(Me.txtid)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.dtp1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.btncancel)
+        Me.Controls.Add(Me.btnok)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Changemovieshows"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CHANGE MOVIE SHOWS"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents btncancel As System.Windows.Forms.Button
+    Friend WithEvents btnok As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents dtp1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents txt1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtid As System.Windows.Forms.TextBox
+    Friend WithEvents cbomoviename As System.Windows.Forms.ComboBox
+    Friend WithEvents cbomovie1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader6 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents txtmid As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+End Class
---
+++ APP/Changemovieshows.vb
@@ -0,0 +1,211 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Changemovieshows
+    Private Sub LoadData()
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+        Dim Lv As ListViewItem
+
+        s_sql = "SELECT     Movie_Schedule.Show_ID, Show_Time.Show_Time, Movie_Details.Movie_Name FROM Movie_Schedule INNER JOIN Movie_Details ON Movie_Schedule.Movie_ID = Movie_Details.Movie_ID INNER JOIN Show_Time ON Movie_Schedule.Show_ID = Show_Time.Show_ID where  mdate ='" & dtp1.Text & "'and movie_details.movie_name='" & cbomoviename.Text & "' and movie_details.active='Yes'"
+
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_Reader = o_cmd.ExecuteReader
+        ListView1.Items.Clear()
+
+        While o_Reader.Read
+            Lv = ListView1.Items.Add(o_Reader("Show_ID") & "")
+            Lv.SubItems.Add(o_Reader("Show_Time") & "")
+            Lv.SubItems.Add(o_Reader("Movie_Name") & "")
+
+        End While
+        o_Reader.Close()
+        txt1.Text = ""
+
+    End Sub
+    Private Sub Addshow_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Connect()
+
+
+        Fillmovie()
+        LoadData()
+
+    End Sub
+
+    Private Sub ListView1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListView1.DoubleClick
+        Dim RNo As Integer
+
+        RNo = ListView1.SelectedItems(0).Text
+
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_reader As SqlDataReader
+
+        s_sql = "select show_time from show_time where show_id=" & Val(RNo)
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_reader = o_cmd.ExecuteReader
+
+
+        If o_reader.Read Then
+            txtid.Text = RNo
+            txt1.Text = o_reader("show_time") & ""
+
+        End If
+
+        o_reader.Close()
+    End Sub
+
+
+    Private Sub ListView1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListView1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btncancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Fillmovie()
+
+        Dim s_Sql As String
+        Dim o_adapter As SqlDataAdapter
+        Dim o_data As Data.DataSet
+
+
+        s_Sql = "SELECT Movie_Details.Movie_Name FROM  Movie_Details INNER Join Movie_Schedule ON Movie_Details.Movie_ID = Movie_Schedule.Movie_ID where movie_schedule.mdate= '" & dtp1.Text & "'group by movie_details.movie_name"
+        o_adapter = New SqlDataAdapter(s_Sql, o_con)
+        o_data = New Data.DataSet
+        o_adapter.Fill(o_data)
+
+        cboMovieName.DataSource = o_data.Tables(0)
+        cboMovieName.DisplayMember = o_data.Tables(0).Columns(0).ColumnName
+        cbomoviename.ValueMember = o_data.Tables(0).Columns(0).ColumnName
+    End Sub
+
+
+
+    Private Sub FillmovieNew()
+
+        Dim s_Sql As String
+        Dim o_adapter As SqlDataAdapter
+        Dim o_data As Data.DataSet
+
+
+        s_Sql = "SELECT Movie_Details.Movie_Name FROM  Movie_Details INNER Join Movie_Schedule ON Movie_Details.Movie_ID = Movie_Schedule.Movie_ID where movie_schedule.mdate= '" & dtp1.Text & "'and movie_details.active='Yes' group by movie_details.movie_name"
+        o_adapter = New SqlDataAdapter(s_Sql, o_con)
+        o_data = New Data.DataSet
+        o_adapter.Fill(o_data)
+
+        cbomovie1.DataSource = o_data.Tables(0)
+        cbomovie1.DisplayMember = o_data.Tables(0).Columns(0).ColumnName
+        cbomovie1.ValueMember = o_data.Tables(0).Columns(0).ColumnName
+    End Sub
+
+    Private Sub cbomoviename_DropDownClosed(ByVal sender As Object, ByVal e As System.EventArgs) Handles cbomoviename.DropDownClosed
+
+    End Sub
+
+
+    Private Sub cbomoviename_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbomoviename.SelectedIndexChanged
+        LoadData()
+        FillmovieNew()
+    End Sub
+
+    Private Sub dtp1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dtp1.ValueChanged
+        LoadData()
+        Fillmovie()
+    End Sub
+
+
+
+
+
+    Private Sub LoadData2()
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+        Dim Lv As ListViewItem
+
+        s_sql = "SELECT     Movie_Schedule.Show_ID, Show_Time.Show_Time, Movie_Details.Movie_Name FROM Movie_Schedule INNER JOIN Movie_Details ON Movie_Schedule.Movie_ID = Movie_Details.Movie_ID INNER JOIN Show_Time ON Movie_Schedule.Show_ID = Show_Time.Show_ID where Movie_Details.Active='yes'and mdate ='" & dtp1.Text & "'and movie_details.movie_name='" & cbomovie1.Text & "'"
+
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_Reader = o_cmd.ExecuteReader
+        ListView2.Items.Clear()
+
+        While o_Reader.Read
+            Lv = ListView2.Items.Add(o_Reader("Show_ID") & "")
+            Lv.SubItems.Add(o_Reader("Show_Time") & "")
+            Lv.SubItems.Add(o_Reader("Movie_Name") & "")
+
+        End While
+        o_Reader.Close()
+    End Sub
+
+
+
+
+    Private Sub cbomovie1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbomovie1.SelectedIndexChanged
+
+        movie2()
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+
+        s_sql = "select movie_id from movie_details where movie_name='" & cbomovie1.Text & "'"
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_Reader = o_cmd.ExecuteReader
+
+        While o_Reader.Read
+            txtmid.Text = o_Reader(0)
+        End While
+        o_Reader.Close()
+
+    End Sub
+
+    Private Sub btnok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnok.Click
+        If txt1.Text = "" Then
+            MsgBox("Select Show Time..")
+            Exit Sub
+
+        End If
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+
+        s_sql = "update movie_schedule set movie_id =" & txtmid.Text & "where show_id=" & txtid.Text
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_Reader = o_cmd.ExecuteReader
+
+        While o_Reader.Read
+            txtmid.Text = o_Reader(0)
+        End While
+        o_Reader.Close()
+        MsgBox(" Record Updated")
+        LoadData2()
+        LoadData()
+
+
+    End Sub
+    Private Sub movie2()
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+        Dim Lv As ListViewItem
+
+        s_sql = "SELECT     Movie_Schedule.Show_ID, Show_Time.Show_Time, Movie_Details.Movie_Name FROM Movie_Schedule INNER JOIN Movie_Details ON Movie_Schedule.Movie_ID = Movie_Details.Movie_ID INNER JOIN Show_Time ON Movie_Schedule.Show_ID = Show_Time.Show_ID where Movie_Details.Active='yes'and mdate ='" & dtp1.Text & "'and movie_details.movie_name='" & cbomovie1.Text & "'"
+
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_Reader = o_cmd.ExecuteReader
+        ListView2.Items.Clear()
+
+        While o_Reader.Read
+            Lv = ListView2.Items.Add(o_Reader("Show_ID") & "")
+            Lv.SubItems.Add(o_Reader("Show_Time") & "")
+            Lv.SubItems.Add(o_Reader("Movie_Name") & "")
+
+        End While
+        o_Reader.Close()
+
+    End Sub
+
+
+End Class

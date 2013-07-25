a8d415af3ee048a0887f1b29d531aa0b3b3ff11b
imsinu9 <imsinu9@gmail.com>
2013-07-25 13:44:15 +0530

    Adding user view form

---
+++ APP/ViewUser.Designer.vb
@@ -0,0 +1,278 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ViewUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtsusername = New System.Windows.Forms.TextBox
+        Me.ListView1 = New System.Windows.Forms.ListView
+        Me.ColumnHeader1 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader2 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader3 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader4 = New System.Windows.Forms.ColumnHeader
+        Me.txtname = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.txtusername = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.txtpassword = New System.Windows.Forms.TextBox
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.txtuserid = New System.Windows.Forms.TextBox
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.btncancel = New System.Windows.Forms.Button
+        Me.btnclear = New System.Windows.Forms.Button
+        Me.btnok = New System.Windows.Forms.Button
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(286, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "USER INFO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 165)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(141, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Search by Username :"
+        '
+        'txtsusername
+        '
+        Me.txtsusername.Location = New System.Drawing.Point(176, 163)
+        Me.txtsusername.Name = "txtsusername"
+        Me.txtsusername.Size = New System.Drawing.Size(155, 20)
+        Me.txtsusername.TabIndex = 3
+        '
+        'ListView1
+        '
+        Me.ListView1.BackColor = System.Drawing.Color.White
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(353, 137)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(439, 266)
+        Me.ListView1.Sorting = System.Windows.Forms.SortOrder.Ascending
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "User_ID"
+        Me.ColumnHeader1.Width = 70
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Name"
+        Me.ColumnHeader2.Width = 135
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Username"
+        Me.ColumnHeader3.Width = 118
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Password"
+        Me.ColumnHeader4.Width = 105
+        '
+        'txtname
+        '
+        Me.txtname.Location = New System.Drawing.Point(176, 251)
+        Me.txtname.Name = "txtname"
+        Me.txtname.Size = New System.Drawing.Size(155, 20)
+        Me.txtname.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(102, 253)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Name :"
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(176, 295)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(155, 20)
+        Me.txtusername.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(76, 297)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 16)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Username :"
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Location = New System.Drawing.Point(176, 339)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.Size = New System.Drawing.Size(155, 20)
+        Me.txtpassword.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(79, 341)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 16)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Password :"
+        '
+        'txtuserid
+        '
+        Me.txtuserid.BackColor = System.Drawing.Color.White
+        Me.txtuserid.Location = New System.Drawing.Point(176, 207)
+        Me.txtuserid.Name = "txtuserid"
+        Me.txtuserid.ReadOnly = True
+        Me.txtuserid.Size = New System.Drawing.Size(155, 20)
+        Me.txtuserid.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(102, 209)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(50, 16)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "UserID"
+        '
+        'btncancel
+        '
+        Me.btncancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncancel.Image = Global.Box_Office.My.Resources.Resources.cancelIcon
+        Me.btncancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btncancel.Location = New System.Drawing.Point(543, 464)
+        Me.btncancel.Name = "btncancel"
+        Me.btncancel.Size = New System.Drawing.Size(95, 33)
+        Me.btncancel.TabIndex = 14
+        Me.btncancel.Text = "     CANCEL"
+        Me.btncancel.UseVisualStyleBackColor = True
+        '
+        'btnclear
+        '
+        Me.btnclear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclear.Image = Global.Box_Office.My.Resources.Resources.icon_refresh
+        Me.btnclear.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnclear.Location = New System.Drawing.Point(364, 464)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(95, 33)
+        Me.btnclear.TabIndex = 13
+        Me.btnclear.Text = "     CLEAR"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'btnok
+        '
+        Me.btnok.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnok.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnok.Image = Global.Box_Office.My.Resources.Resources.Symbol_HookOK_18px
+        Me.btnok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnok.Location = New System.Drawing.Point(185, 464)
+        Me.btnok.Name = "btnok"
+        Me.btnok.Size = New System.Drawing.Size(95, 33)
+        Me.btnok.TabIndex = 12
+        Me.btnok.Text = "OK"
+        Me.btnok.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Office.My.Resources.Resources.Inox_Leisure
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 16)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(88, 93)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ViewUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(823, 529)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.txtuserid)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btncancel)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.btnok)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtname)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.txtsusername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "ViewUser"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "VIEW USERS"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtsusername As System.Windows.Forms.TextBox
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents txtname As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtusername As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtpassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btncancel As System.Windows.Forms.Button
+    Friend WithEvents btnclear As System.Windows.Forms.Button
+    Friend WithEvents btnok As System.Windows.Forms.Button
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents txtuserid As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+End Class
---
+++ APP/ViewUser.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Data.SqlClient
+
+Public Class ViewUser
+    Dim Status As String
+
+    Private Sub LoadData()
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_Reader As SqlDataReader
+        Dim Lv As ListViewItem
+
+        s_sql = "select User_ID, Name, username, password from login where username like'" & txtsusername.Text & "%'"
+
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_Reader = o_cmd.ExecuteReader
+        ListView1.Items.Clear()
+
+        While o_Reader.Read
+            Lv = ListView1.Items.Add(o_Reader("User_ID") & "")
+            Lv.SubItems.Add(o_Reader("Name") & "")
+            Lv.SubItems.Add(o_Reader("Username") & "")
+            Lv.SubItems.Add(o_Reader("Password") & "")
+        End While
+        o_Reader.Close()
+    End Sub
+
+    Private Sub clear()
+        txtsusername.Text = ""
+        txtname.Text = ""
+        txtusername.Text = ""
+        txtpassword.Text = ""
+        txtuserid.Text = ""
+        txtsusername.Focus()
+
+        btnok.Enabled = False
+
+    End Sub
+
+    Private Sub ViewUser_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            SendKeys.Send("{TAB}")
+
+        End If
+    End Sub
+    
+
+    Private Sub ViewUser_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Connect()
+        LoadData()
+
+        btnok.Enabled = False
+
+    End Sub
+
+    Private Sub btnclear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnclear.Click
+        clear()
+
+    End Sub
+
+    
+    Private Sub btncancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncancel.Click
+        administator.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub txtname_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtname.TextChanged
+        If txtname.Text = "" Or txtpassword.Text = "" Or txtusername.Text = "" Then
+            btnok.Enabled = False
+        Else
+            btnok.Enabled = True
+
+        End If
+    End Sub
+
+    Private Sub txtusername_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtusername.TextChanged
+        If txtname.Text = "" Or txtpassword.Text = "" Or txtusername.Text = "" Then
+            btnok.Enabled = False
+        Else
+            btnok.Enabled = True
+
+        End If
+    End Sub
+
+    Private Sub txtpassword_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtpassword.TextChanged
+        If txtname.Text = "" Or txtpassword.Text = "" Or txtusername.Text = "" Then
+            btnok.Enabled = False
+        Else
+            btnok.Enabled = True
+
+        End If
+    End Sub
+
+    Private Sub btnok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnok.Click
+        Dim s_Sql As String
+        Dim o_cmd As SqlCommand
+
+        s_Sql = "Update login set Name=@Name,  password=@password where User_ID='" & Val(txtuserid.Text) & "'"
+        o_cmd = New SqlCommand(s_Sql, o_con)
+        o_cmd.Parameters.Add(New SqlParameter("@Name", txtname.Text))
+        o_cmd.Parameters.Add(New SqlParameter("@username", txtusername.Text))
+        o_cmd.Parameters.Add(New SqlParameter("@password", txtpassword.Text))
+        
+        o_cmd.ExecuteNonQuery()
+
+        MsgBox("Record Updated")
+
+        clear()
+        LoadData()
+    End Sub
+
+    Private Sub ListView1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListView1.DoubleClick
+
+        Dim user As String
+        user = ListView1.SelectedItems(0).Text
+        Dim s_sql As String
+        Dim o_cmd As SqlCommand
+        Dim o_reader As SqlDataReader
+        s_sql = " Select * from login where User_ID='" & Val(user) & "'"
+        o_cmd = New SqlCommand(s_sql, o_con)
+        o_reader = o_cmd.ExecuteReader
+
+        If o_reader.Read Then
+            txtuserid.Text = Val(user)
+            txtname.Text = o_reader("Name") & ""
+            txtusername.Text = o_reader("Username") & ""
+            txtpassword.Text = o_reader("Password") & ""
+        End If
+
+        o_reader.Close()
+    End Sub
+
+    
+    Private Sub txtsusername_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtsusername.TextChanged
+        LoadData()
+
+    End Sub
+
+    
+End Class

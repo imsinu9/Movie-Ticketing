686da2a27cd25dcba17c33b630a8ccc865e578f1
imsinu9 <imsinu9@gmail.com>
2013-07-25 13:39:53 +0530

    adding new user form

---
+++ APP/NewUser.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class NewUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.txtName = New System.Windows.Forms.TextBox
+        Me.txtUserName = New System.Windows.Forms.TextBox
+        Me.txtPassword = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.txtconfrmpass = New System.Windows.Forms.TextBox
+        Me.Label5 = New System.Windows.Forms.Label
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(137, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(154, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CREATE NEW USER"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(83, 127)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(54, 165)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "UserName :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(60, 203)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Password :"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(173, 124)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(174, 20)
+        Me.txtName.TabIndex = 2
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(173, 162)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(174, 20)
+        Me.txtUserName.TabIndex = 4
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(173, 200)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(174, 20)
+        Me.txtPassword.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(25, 285)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 31)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "CREATE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(142, 285)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 31)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "RESET"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(260, 285)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(87, 31)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "CANCEL"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Box_Office.My.Resources.Resources.Inox_Leisure
+        Me.PictureBox1.Location = New System.Drawing.Point(23, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(89, 92)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtconfrmpass
+        '
+        Me.txtconfrmpass.Location = New System.Drawing.Point(173, 238)
+        Me.txtconfrmpass.Name = "txtconfrmpass"
+        Me.txtconfrmpass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtconfrmpass.Size = New System.Drawing.Size(174, 20)
+        Me.txtconfrmpass.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 239)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(122, 16)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Confirm Password :"
+        '
+        'NewUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(386, 328)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.txtconfrmpass)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "NewUser"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "NEW USER"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents txtconfrmpass As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+End Class
---
+++ APP/NewUser.vb
@@ -0,0 +1,116 @@
+﻿Imports System.Data.SqlClient
+Public Class NewUser
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        txtName.Text = ""
+        txtUserName.Text = ""
+        txtPassword.Text = ""
+        txtconfrmpass.Text = ""
+        txtName.Focus()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        If Len(txtPassword.Text) < 6 Then
+            MsgBox("Password too short,Enter minimumu 6 characters")
+            txtPassword.Text = ""
+            txtconfrmpass.Text = ""
+            txtPassword.Focus()
+            Exit Sub
+        End If
+        If Len(txtconfrmpass.Text) < 6 Then
+            MsgBox("Password too short,Enter minimumu 6 characters")
+            txtPassword.Text = ""
+            txtconfrmpass.Text = ""
+            txtPassword.Focus()
+
+        End If
+
+
+        If txtPassword.Text <> txtconfrmpass.Text Then
+            txtPassword.Text = ""
+            txtconfrmpass.Text = ""
+            txtPassword.Focus()
+            MsgBox("Enter same Password")
+            Exit Sub
+        End If
+
+
+        Dim s_Sql As String
+        Dim o_cmd As SqlCommand
+
+        s_Sql = "Insert into Login(Name,username,password) values(@Name,@username,@password)"
+        o_cmd = New SqlCommand(s_Sql, o_con)
+
+        o_cmd.Parameters.Add(New SqlParameter("@Name", txtName.Text))
+        o_cmd.Parameters.Add(New SqlParameter("@username", txtUserName.Text))
+        o_cmd.Parameters.Add(New SqlParameter("@password", txtPassword.Text))
+        o_cmd.ExecuteNonQuery()
+        MsgBox("New record Created..")
+        Me.Close()
+        administator.Show()
+    End Sub
+
+    Private Sub NewUser_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Connect()
+        Button1.Enabled = False
+
+    End Sub
+
+    Private Sub txtName_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtName.KeyPress
+
+        If e.KeyChar >= "a" And e.KeyChar <= "z" Or e.KeyChar >= "A" And e.KeyChar <= "Z" Or Asc(e.KeyChar) = 32 Or Asc(e.KeyChar) = 8 Then
+            e.KeyChar = e.KeyChar
+
+        Else
+            e.KeyChar = ""
+        End If
+
+    End Sub
+
+    Private Sub txtName_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtName.TextChanged
+        If txtName.Text = "" Or txtUserName.Text = "" Or txtPassword.Text = "" Or txtconfrmpass.Text = "" Then
+            Button1.Enabled = False
+        Else
+            Button1.Enabled = True
+
+        End If
+
+    End Sub
+
+    Private Sub txtUserName_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtUserName.TextChanged
+        If txtName.Text = "" Or txtUserName.Text = "" Or txtPassword.Text = "" Or txtconfrmpass.Text = "" Then
+            Button1.Enabled = False
+        Else
+            Button1.Enabled = True
+
+        End If
+
+    End Sub
+
+    Private Sub txtPassword_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtPassword.TextChanged
+        If txtName.Text = "" Or txtUserName.Text = "" Or txtPassword.Text = "" Or txtconfrmpass.Text = "" Then
+            Button1.Enabled = False
+        Else
+            Button1.Enabled = True
+
+        End If
+
+
+    End Sub
+
+    Private Sub txtconfrmpass_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtconfrmpass.TextChanged
+        If txtName.Text = "" Or txtUserName.Text = "" Or txtPassword.Text = "" Or txtconfrmpass.Text = "" Then
+            Button1.Enabled = False
+        Else
+            Button1.Enabled = True
+
+        End If
+
+    End Sub
+End Class
